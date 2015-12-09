8e2094a69252d3c7c1f609873c218b1c16ebe700
Mateusz Ksiazek <ksiazek.mate@gmail.com>
2015-12-09 14:37:58 +0100

    Added Twig template engine

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'hello world';
+require_once '../vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('../src/mvc/views');
+$twig = new Twig_Environment($loader, array(
+    //'cache' => '../cache',
+    'cache' => false
+));
+
+echo $twig->render('index.html.twig', array('name' => 'Mateusz'));
