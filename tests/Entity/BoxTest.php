6534d4058baa4a972df157a03c49d0e066cac6f7
Mateusz Książek <ksiazek.mate@gmail.com>
2017-04-14 23:19:31 +0200

    Add missed tests

--- tests/Entity/BoxTest.php
+++ tests/Entity/BoxTest.php
@@ -7,14 +7,49 @@ use Krauza\Core\ValueObject\EntityId;
 class BoxTest extends PHPUnit_Framework_TestCase
 {
     /**
+     * @var Box
+     */
+    private $box;
+
+    public function setUp()
+    {
+        $this->box = new Box(new BoxName('name'));
+        $this->box->setId(new EntityId('1'));
+    }
+    /**
      * @test
      */
     public function boxShouldBeCreatable()
     {
-        $card = new Box(new BoxName('name'));
-        $card->setId(new EntityId('1'));
+        $this->assertEquals('name', $this->box->getName());
+        $this->assertEquals('1', $this->box->getId());
+    }
 
-        $this->assertEquals('name', $card->getName());
-        $this->assertEquals('1', $card->getId());
+    /**
+     * @test
+     */
+    public function shouldIncrementSection()
+    {
+        $this->box->incrementCurrentSection();
+        $this->assertEquals(1, $this->box->getCurrentSection());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldRewindToFirstSection()
+    {
+        $box = new Box(new BoxName('name'), 3);
+        $box->rewindToFirstSection();
+        $this->assertEquals(0, $box->getCurrentSection());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSetSection()
+    {
+        $this->box->setCurrentSection(4);
+        $this->assertEquals(4, $this->box->getCurrentSection());
     }
 }
