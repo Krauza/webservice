fcefdb503eec4889c174715f62f9e893028b14b1
Mateusz Książek <ksiazek.mate@gmail.com>
2017-05-18 17:24:46 +0200

    Fix query

--- src/Infrastructure/Api/Type/QueryType.php
+++ src/Infrastructure/Api/Type/QueryType.php
@@ -4,10 +4,13 @@ namespace Krauza\Infrastructure\Api\Type;
 
 use GraphQL\Type\Definition\Type;
 use GraphQL\Type\Definition\ObjectType;
+use Krauza\Core\UseCase\AdjustFirstSection;
 use Krauza\Core\UseCase\FindNextCard as FindNextCardUseCase;
+use Krauza\Core\UseCase\SetCurrentSection;
 use Krauza\Infrastructure\Api\Action\FindNextCard;
 use Krauza\Infrastructure\Api\TypeRegistry;
 use Krauza\Infrastructure\DataAccess\BoxRepository;
+use Krauza\Infrastructure\DataAccess\BoxSectionsRepository;
 use Krauza\Infrastructure\DataAccess\CardRepository;
 
 class QueryType extends ObjectType
@@ -30,14 +33,17 @@ class QueryType extends ObjectType
                         return BoxType::objectToArray($boxRepository->getById($args['id']));
                     }
                 ],
-                'card' => [
+                'nextCard' => [
                     'type' => TypeRegistry::getCardType(),
                     'args' => [],
                     'resolve' => function ($rootValue, $args, $context) {
                         $boxRepository = new BoxRepository($context['database_connection']);
+                        $boxSectionsRepository = new BoxSectionsRepository($context['database_connection']);
                         $cardRepository = new CardRepository($context['database_connection']);
-                        $nextCardUseCase = new FindNextCardUseCase($boxRepository, $cardRepository);
-                        $nextCard = new FindNextCard($nextCardUseCase, $boxRepository);
+                        $nextCardUseCase = new FindNextCardUseCase($boxSectionsRepository, $cardRepository);
+                        $adjustFirstSection = new AdjustFirstSection($boxSectionsRepository);
+                        $setCurrentSection = new SetCurrentSection($boxRepository, $boxSectionsRepository);
+                        $nextCard = new FindNextCard($nextCardUseCase, $adjustFirstSection, $setCurrentSection, $boxRepository);
                         return $nextCard->action($args);
                     }
                 ]
